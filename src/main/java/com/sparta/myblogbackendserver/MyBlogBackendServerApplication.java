af2c989f08fa7f3bde3f11562958b87ccdbea3b0
SeungyeopHong <milnil0228@gmail.com>
2022-12-07 11:38:58 +0900

    <docs>:other files

---
+++ src/main/java/com/sparta/myblogbackendserver/MyBlogBackendServerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.myblogbackendserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlogBackendServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBlogBackendServerApplication.class, args);
+    }
+
+}
