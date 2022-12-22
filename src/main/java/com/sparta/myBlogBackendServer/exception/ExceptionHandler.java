76d28a91b3fd3c960c3d099a620b66435d149832
hong-seungyeop <milnil0228@gmail.com>
2022-12-22 20:30:43 +0900

    @Valid 예외 처리 메시지만 추가

--- src/main/java/com/sparta/myBlogBackendServer/exception/ExceptionHandler.java
+++ src/main/java/com/sparta/myBlogBackendServer/exception/ExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.sparta.myBlogBackendServer.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,5 +14,11 @@ public class ExceptionHandler {
     @org.springframework.web.bind.annotation.ExceptionHandler(IllegalArgumentException.class)
     public String handlerIllegalArgumentException(IllegalArgumentException e) {
         return  e.getMessage();
+    }
+
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
+    public String handlerMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return e.getMessage();
     }
 }
